c971b6e1971439acea7c0b32b114f8b5e9d04775
Scott Batson <sjbats@email.com>
2017-06-19 14:31:44 -0400

    add chatting step-through

--- app/components/modals/chat-dialogue.js
+++ app/components/modals/chat-dialogue.js
@@ -1,4 +1,49 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  Component,
+  computed,
+  computed: { alias, equal },
+  get
+} = Ember;
+
+export default Component.extend({
+  chatIndex: 0,
+
+  chatMessages: [
+    {
+      speaker: 'primary',
+      text: 'This is the first message',
+      emotion: 'happy'
+    }, {
+      speaker: 'secondary',
+      text: 'And this is second',
+      emotion: 'sad'
+    }, {
+      speaker: 'primary',
+      text: 'Do not do that!',
+      emotion: 'angry'
+    }, {
+      speaker: 'secondary',
+      text: 'What a big mistake!!',
+      emotion: 'scared'
+    }
+  ],
+
+  currentChat: computed('chatIndex', 'chatMessages.[]', function() {
+    let index = get(this, 'chatIndex');
+    return get(this, 'chatMessages')[index];
+  }),
+
+  currentText: alias('currentChat.text'),
+  currentSpeaker: alias('currentChat.speaker'),
+
+  isPrimary: equal('currentSpeaker', 'primary'),
+  isSecondary: equal('currentSpeaker', 'secondary'),
+
+  actions: {
+    stepForward() {
+      this.incrementProperty('chatIndex');
+    }
+  }
 });
